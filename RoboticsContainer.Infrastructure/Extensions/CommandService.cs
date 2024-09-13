e9b24bc0ceff804324014af75b8ba8327a2513c1
Jacob <jacobwalker130959@outlook.com>
2024-09-13 09:51:20 +0100

    clearing things up

--- RoboticsContainer.Infrastructure/Extensions/CommandService.cs
+++ RoboticsContainer.Infrastructure/Extensions/CommandService.cs
@@ -9,6 +9,7 @@ namespace RoboticsContainer.Infrastructure.Extensions
 {
     public class CommandService : ICommandService
     {
+        // Wrapper to moq extension methods
         public async Task<(string Output, string Error)> ExecuteCommandAsync(string fileName, string arguments, bool waitForExit = true)
         {
             return await ProcessExtensions.ExecuteCommandAsync(fileName, arguments, waitForExit);
